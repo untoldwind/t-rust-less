389a5e1961923ac4fb45ac94b84210e8d69527ee
Bodo Junglas <junglas@objectcode.de>
2019-05-20 12:07:35 +0200

    Handle otp period correctly

--- cli/src/view/secret_totp_view.rs
+++ cli/src/view/secret_totp_view.rs
@@ -11,8 +11,7 @@ pub struct SecretTOTPView {
   otp_url: ZeroingString,
   token_display_id: String,
   token_valid_id: String,
-  valid_until: Option<u64>,
-  period: Option<u64>,
+  maybe_valid_until: Option<u64>,
 }
 
 impl SecretTOTPView {
@@ -20,24 +19,42 @@ impl SecretTOTPView {
   where
     F: Fn(&mut Cursive) -> () + 'static,
   {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+    let (token, maybe_valid_until, maybe_period) = match OTPAuthUrl::parse(&otp_url) {
+      Ok(otpauth) => {
+        let (token, valid_until) = otpauth.generate(now);
+        let period = match otpauth.otp_type {
+          OTPType::TOTP { period } => Some(period),
+          _ => None,
+        };
+        (token, Some(valid_until), period)
+      }
+      _ => ("".to_string(), None, None),
+    };
     let token_display_id = format!("token_display_{}", property);
     let token_valid_id = format!("token_valid_{}", property);
+    let mut token_display =
+      LinearLayout::vertical().child(TextView::new(token).with_id(token_display_id.clone()).full_width());
+
+    if let Some(period) = maybe_period {
+      token_display = token_display.child(
+        ProgressBar::new()
+          .max(period as usize)
+          .with_id(token_valid_id.clone())
+          .full_width(),
+      )
+    }
 
     SecretTOTPView {
       base_view: LinearLayout::horizontal()
         .child(TextView::new(format!("{:10}: ", property)))
-        .child(
-          LinearLayout::vertical()
-            .child(TextView::new("").with_id(token_display_id.clone()).full_width())
-            .child(ProgressBar::new().max(30).with_id(token_valid_id.clone()).full_width())
-            .full_width(),
-        )
+        .child(token_display.full_width())
         .child(Button::new("Copy", on_copy)),
       otp_url: otp_url.to_zeroing(),
       token_display_id,
       token_valid_id,
-      valid_until: None,
-      period: None,
+      maybe_valid_until,
     }
   }
 }
@@ -57,24 +74,19 @@ impl ViewWrapper for SecretTOTPView {
     F: FnOnce(&mut Self::V) -> R,
   {
     let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-    if self.valid_until.is_none() || self.valid_until.unwrap() <= now {
-      match OTPAuthUrl::parse(&self.otp_url) {
-        Ok(otpauth) => {
-          let (token, valid_until) = otpauth.generate(now);
-          let mut token_display = self.base_view.find_id::<TextView>(&self.token_display_id).unwrap();
-          token_display.set_content(token);
-          self.valid_until = Some(valid_until);
-          self.period = match otpauth.otp_type {
-            OTPType::TOTP { period } => Some(u64::from(period)),
-            _ => None,
+    if let Some(valid_until) = self.maybe_valid_until {
+      if valid_until <= now {
+        match OTPAuthUrl::parse(&self.otp_url) {
+          Ok(otpauth) => {
+            let (token, valid_until) = otpauth.generate(now);
+            let mut token_display = self.base_view.find_id::<TextView>(&self.token_display_id).unwrap();
+            token_display.set_content(token);
+            self.maybe_valid_until = Some(valid_until);
           }
+          _ => (),
         }
-        _ => (),
       }
-    }
-    if let Some(period) = self.period {
-      let mut token_valid = self.base_view.find_id::<ProgressBar>(&self.token_valid_id).unwrap();
-      if let Some(valid_until) = self.valid_until {
+      if let Some(mut token_valid) = self.base_view.find_id::<ProgressBar>(&self.token_valid_id) {
         token_valid.set_value((valid_until - now) as usize);
       }
     }
