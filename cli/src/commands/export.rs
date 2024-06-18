2fcf909f1880420c09b383084e0c10dfb8de48f9
Bodo Junglas <junglas@objectcode.de>
2024-06-18 20:40:51 +0200

    Implement export

--- cli/src/commands/export.rs
+++ cli/src/commands/export.rs
@@ -1,10 +1,14 @@
-use std::sync::Arc;
+use std::{
+  fs::File,
+  io::{stdout, Write},
+  sync::Arc,
+};
 
 use anyhow::{Context, Result};
 use clap::Args;
 use t_rust_less_lib::{api::SecretListFilter, service::TrustlessService};
 
-use crate::error::ExtResult;
+use crate::{error::ExtResult, model::import_v2::SecretV2};
 
 use super::{tui::create_tui, unlock_store};
 
@@ -12,6 +16,12 @@ use super::{tui::create_tui, unlock_store};
 pub struct ExportCommand {
   #[clap(help = "File to export to. If not set export will write to stdout")]
   pub file: Option<String>,
+
+  #[clap(long)]
+  pub include_deleted: bool,
+
+  #[clap(long)]
+  pub include_version: bool,
 }
 
 impl ExportCommand {
@@ -27,20 +37,63 @@ impl ExportCommand {
       siv.quit();
     }
 
-    let list = secrets_store.list(&SecretListFilter {
+    let mut filters = vec![SecretListFilter {
       name: None,
       url: None,
       tag: None,
       ..Default::default()
-    })?;
+    }];
 
-    for entry_match in &list.entries {
-      let secret = secrets_store
-        .get(&entry_match.entry.id)
-        .with_context(|| format!("Get entry {} {}", entry_match.entry.id, entry_match.entry.name))?;
-
-      println!("{}", secret.id);
+    if self.include_deleted {
+      filters.push(SecretListFilter {
+        name: None,
+        url: None,
+        tag: None,
+        deleted: true,
+        ..Default::default()
+      })
     }
+
+    let mut export_stream: Box<dyn Write> = match &self.file {
+      Some(file_name) => {
+        let file = File::open(file_name).with_context(|| format!("Failed opening {}", file_name))?;
+        Box::new(file)
+      }
+      None => Box::new(stdout()),
+    };
+
+    for filter in &filters {
+      let list = secrets_store.list(filter)?;
+
+      for entry_match in &list.entries {
+        let secret = secrets_store
+          .get(&entry_match.entry.id)
+          .with_context(|| format!("Get entry {} {}", entry_match.entry.id, entry_match.entry.name))?;
+
+        let mut service_v2 = SecretV2 {
+          id: secret.id.clone(),
+          current: (&secret.current).into(),
+          versions: vec![],
+        };
+
+        if self.include_version {
+          for version_ref in &secret.versions {
+            let version = secrets_store.get_version(&version_ref.block_id).with_context(|| {
+              format!(
+                "Get entry version {} {} {:?}",
+                entry_match.entry.id, entry_match.entry.name, &version_ref.timestamp
+              )
+            })?;
+
+            service_v2.versions.push((&version).into());
+          }
+        }
+
+        serde_json::to_writer(&mut export_stream, &service_v2)?;
+        writeln!(&mut export_stream)?;
+      }
+    }
+    export_stream.flush()?;
 
     Ok(())
   }
--- cli/src/model/import_v2.rs
+++ cli/src/model/import_v2.rs
@@ -1,1 +1,47 @@
+use serde::{Deserialize, Serialize};
+use t_rust_less_lib::api::{SecretAttachment, SecretProperties, SecretType, SecretVersion, ZeroizeDateTime};
+use zeroize::Zeroize;
 
+#[derive(Debug, Serialize, Deserialize, Zeroize)]
+#[zeroize(drop)]
+pub struct SecretV2 {
+  pub id: String,
+  pub current: SecretVersionV2,
+  pub versions: Vec<SecretVersionV2>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Zeroize)]
+#[zeroize(drop)]
+pub struct SecretVersionV2 {
+  #[serde(rename = "type")]
+  pub secret_type: SecretType,
+  pub timestamp: ZeroizeDateTime,
+  pub name: String,
+  #[serde(default)]
+  pub tags: Vec<String>,
+  #[serde(default)]
+  pub urls: Vec<String>,
+  pub properties: SecretProperties,
+  #[serde(default)]
+  pub attachments: Vec<SecretAttachment>,
+  #[serde(default)]
+  pub deleted: bool,
+  #[serde(default)]
+  pub recipients: Vec<String>,
+}
+
+impl From<&SecretVersion> for SecretVersionV2 {
+  fn from(value: &SecretVersion) -> Self {
+    SecretVersionV2 {
+      secret_type: value.secret_type,
+      timestamp: value.timestamp,
+      name: value.name.clone(),
+      tags: value.tags.clone(),
+      urls: value.urls.clone(),
+      properties: value.properties.clone(),
+      attachments: value.attachments.clone(),
+      deleted: value.deleted,
+      recipients: value.recipients.clone(),
+    }
+  }
+}
